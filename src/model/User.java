b1c951bb6793e32614fde22d6fb90d28e609f22f
angel <jjjk>
2023-09-03 13:54:54 +0200

    wip

--- src/model/User.java
+++ src/model/User.java
@@ -1,11 +1,20 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class User {
 
     private String name;
-    private List<User> userList;
+    private List<User> followingList;
     private List <Post> postList;
 
+    public User() {
+    }
+
+    public User(String name) {
+        this.name = name;
+        this.followingList = new ArrayList<User>();
+        this.postList = new ArrayList<Post>();
+    }
 }
