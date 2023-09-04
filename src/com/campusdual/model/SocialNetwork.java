6dcb3e707ce7f84b964d6ebe48ba003e01c92af2
angel <jjjk>
2023-09-04 14:36:56 +0200

    wip main

---
+++ src/com/campusdual/model/SocialNetwork.java
@@ -0,0 +1,18 @@
+package com.campusdual;
+
+import com.campusdual.model.User;
+
+import java.util.List;
+
+public class SocialNetwork {
+    private List <User> signedUpUsers;
+
+    public void addNewUserComment(Object obj, List list) {
+
+       if (obj instanceof User){
+
+        signedUpUsers.add(user);
+    }
+
+}
+}
